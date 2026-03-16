c0ef9c7c17c88d14824e1836ac72c4c693e72e6a
tilka.na <nadia.arrahime@gmail.com>
2026-03-16 16:44:37 +0100

    1er commit

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS flaskdb;
+USE flaskdb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+email text 
+);
+
+INSERT INTO users (name) VALUES ('Salim'), ('Nour'), ('Youssef');
